780299e31695d17e4517821fc6d1c03369200cc2
CubecraftzDev <igfunnies2013@yahoo.com>
2013-12-13 13:15:31 -0800

    Create Configurationprovider.java

---
+++ package-net.md_5.bungee.config-import-java.io.File-import-java.io.IOException-import-java.io.Reader-import-java.io.Writer-import-java.util.HashMap-import-java.util.Map-public-abstract-class-ConfigurationProvider-private-static-final-Map-Class-extends-ConfigurationProvider-ConfigurationProvider-providers-new-HashMap-static-providers.put-YamlConfiguration.class-new-YamlConfiguration-public-static-ConfigurationProvider-getProvider-Class-extends-ConfigurationProvider-provider-return-providers.get-provider/Configurationprovider.java
@@ -0,0 +1,35 @@
+package net.CubeCraftz.java.Lobbyjoin.config;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class ConfigurationProvider
+{
+
+    private static final Map<Class<? extends ConfigurationProvider>, ConfigurationProvider> providers = new HashMap<>();
+
+    static
+    {
+        providers.put( YamlConfiguration.class, new YamlConfiguration() );
+    }
+
+    public static ConfigurationProvider getProvider(Class<? extends ConfigurationProvider> provider)
+    {
+        return providers.get( provider );
+    }
+    /*------------------------------------------------------------------------*/
+
+    public abstract void save(Configuration config, File file) throws IOException;
+
+    public abstract void save(Configuration config, Writer writer);
+
+    public abstract Configuration load(File file) throws IOException;
+
+    public abstract Configuration load(Reader reader);
+
+    public abstract Configuration load(String string);
+}
